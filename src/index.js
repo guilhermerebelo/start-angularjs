aad5611cef70363e74647db33e25f9b90d6fb239
Guilherme Rebelo <guilhermerebelo@live.com>
2019-10-31 21:47:28 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ var angular = require('angular');
 require('./componente-modelo.css');
 
 module.exports = angular
-    .module('pes-components.pes-componente-modelo', [])
+    .module('componente-modelo', [])
     .directive('componenteModelo', require('./componente-modelo.directive')).name;
